f5fd6c6b817c2f4a935bfca8614e4ddf4881da01
SeoClick <seoclick.by@gmail.com>
2022-05-25 17:56:59 +0300

    add main img

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Main.module.css'
 import commonStyle from '../../common/styles/Container.module.css'
-import mainPhoto from '../../assets/images/main_photo.png'
+import realPhoto from '../../assets/images/myself-real3.png'
+import digitalPhoto from '../../assets/images/myself-digital.png'
+
 
 const Main = () => {
+    let[isReal,setReal] = useState(true);
+
+    const changeImg = () => {
+        setReal(!isReal);
+    }
+
     return (
         <div className={style.main}>
             <div className={commonStyle.container}>
@@ -12,8 +20,8 @@ const Main = () => {
                     <h1>I am Dmitry Zagluhinsky</h1>
                     <p>A FRONT-END DEVELOPER</p>
                 </div>
-                <div className={style.photo}>
-                    <img src={mainPhoto} alt=""/>
+                <div className={style.photo} onClick={changeImg}>
+                    <img src={isReal ? realPhoto: digitalPhoto} alt=""/>
                 </div>
             </div>
         </div>
