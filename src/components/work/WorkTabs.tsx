cc613f296273749afc3539725954914dc2f2d77d
3admitry <3a.dmitriy@gmail.com>
2022-07-10 03:38:57 +0300

    modify WorkTabs.tsx experimental Tabs mui

--- src/components/work/WorkTabs.tsx
+++ src/components/work/WorkTabs.tsx
@@ -1,94 +1,41 @@
 import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import style from './Work.module.css'
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import style from './Work.module.css';
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
+export default function LabTabs() {
+    const [value, setValue] = React.useState('1');
 
-function TabPanel(props: TabPanelProps) {
-    const {children, value, index, ...other} = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{p: 3}}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: number) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
-
-export default function WorkTabs() {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
 
     return (
-        <Box sx={{width: '100%', /*bgcolor: 'white'*/}}>
-            <Box className={'workTabs'} sx={{borderBottom: 1, borderColor: 'var(--dark-slate)'}}>
-                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
-                    <Tab className={'customTab'} label="React/Redux" {...a11yProps(0)} />
-                    <Tab className={'customTab'} label="JS" {...a11yProps(1)} />
-                    <Tab className={'customTab'} label="HTML/CSS" {...a11yProps(2)} />
-                    <Tab className={'customTab'} label="PHP" {...a11yProps(3)} />
-                    <Tab className={'customTab'} label="SEO" {...a11yProps(4)} />
-                    <Tab className={'customTab'} label="Design" {...a11yProps(5)} />
-                </Tabs>
-            </Box>
-            <div className={style.workBoxs}>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={0}>
-                    Item One
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    Item two
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    Item two
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    Item Three
-                </TabPanel>
-            </div>
+        <Box sx={{width: '100%', typography: 'body1'}}>
+            <TabContext value={value}>
+                <Box className={'workTabs'} sx={{borderBottom: 1, borderColor: 'var(--dark-slate)'}}>
+                    <TabList onChange={handleChange} aria-label="work tabs">
+                        <Tab className={'customTab'} label="All" value="all"/>
+                        <Tab className={'customTab'} label="React/Redux" value="1"/>
+                        <Tab className={'customTab'} label="HTML/CSS" value="2"/>
+                        <Tab className={'customTab'} label="PHP" value="3"/>
+                        <Tab className={'customTab'} label="SEO" value="4"/>
+                        <Tab className={'customTab'} label="Design" value="5"/>
+                        <Tab className={'customTab'} label="All" value="6"/>
+                    </TabList>
+                </Box>
+                <div className={style.workBoxs}>
+                    <TabPanel value={value === 'all' ? 'all' : '1'}>Item One</TabPanel>
+                    <TabPanel value={value === 'all' ? 'all' : '2'}>Item Two</TabPanel>
+                    <TabPanel value={value === 'all' ? 'all' : '3'}>Item Three</TabPanel>
+                    <TabPanel value={value === 'all' ? 'all' : '4'}>Item 4</TabPanel>
+                    <TabPanel value={value === 'all' ? 'all' : '5'}>Item 5</TabPanel>
+                    <TabPanel value={value === 'all' ? 'all' : '6'}>Item 6</TabPanel>
+                </div>
+            </TabContext>
         </Box>
     );
 }
